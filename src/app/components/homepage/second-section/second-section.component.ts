ca867035da71d4acfcdc17e61f4063a1c01097f8
Youssef-14 <youssefmaatoug99@gmail.com>
2023-12-28 23:45:14 +0100

    feat : IHM-Implementation

--- src/app/components/homepage/second-section/second-section.component.ts
+++ src/app/components/homepage/second-section/second-section.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {QRCodeModule} from "angularx-qrcode";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-second-section',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, QRCodeModule, FormsModule],
   templateUrl: './second-section.component.html',
   styleUrl: './second-section.component.css'
 })
 export class SecondSectionComponent {
+  text: string="https://copilot.microsoft.com/";
+  inputValue: string="";
 
+  constructor() {
+  }
+
+  generateQRCode() {
+    if (this.inputValue == null || this.inputValue == "")
+      alert("Please enter a valid URL")
+    else
+    this.text = this.inputValue;
+  }
 }
