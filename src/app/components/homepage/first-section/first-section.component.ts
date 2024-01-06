7e58f3a3ab563388acd4749d3b3dae5f1166017a
Youssef-14 <youssefmaatoug99@gmail.com>
2024-01-06 23:47:40 +0100

    feat : AddingFeatures

--- src/app/components/homepage/first-section/first-section.component.ts
+++ src/app/components/homepage/first-section/first-section.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-first-section',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './first-section.component.html',
   styleUrl: './first-section.component.css'
 })
